f840024844301a094dd10591a0c6c4b0317ab1d0
alt4 <north4mp@gmail.com>
2025-04-12 14:55:47 +0000

    add postgres support

--- src/db/client.go
+++ src/db/client.go
@@ -1,12 +1,13 @@
-// client.go
 package db
 
 import (
 	"context"
 	"log"
+	"strings"
 
 	"entgo.io/ent/dialect"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 
 	"github.com/pluja/anysub/ent"
 	"github.com/pluja/anysub/utils"
@@ -17,16 +18,33 @@ var client *ent.Client
 // Init initializes the database client.
 func Init() {
 	var err error
-	//client, err = ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
-	// client, err = ent.Open(dialect.SQLite, "file:./db.sql?cache=shared&_fk=1")
-	client, err = ent.Open(dialect.MySQL, utils.Getenv("DATABASE_URI", "anysub:anysub@tcp(database:3306)/anysub?parseTime=True"))
+
+	// Get the full DSN and type from env
+	dsn := utils.Getenv("DATABASE_URI", "anysub:anysub@tcp(database:3306)/anysub?parseTime=True")
+	driver := detectDriver(dsn)
+
+	// Open the appropriate dialect
+	client, err = ent.Open(driver, dsn)
 	if err != nil {
-		log.Fatalf("failed opening connection to sqlite: %v", err)
+		log.Fatalf("failed opening connection to %s: %v", driver, err)
 	}
 
 	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
+
+// detectDriver attempts to detect the DB dialect from the DSN
+func detectDriver(dsn string) string {
+	switch {
+	case strings.HasPrefix(dsn, "postgres://") || strings.HasPrefix(dsn, "postgresql://"):
+		return dialect.Postgres
+	case strings.Contains(dsn, "@tcp("):
+		return dialect.MySQL
+	default:
+		log.Fatalf("unsupported or undetected database dialect in DSN: %s", dsn)
+		return ""
 	}
 }
 
--- src/go.mod
+++ src/go.mod
@@ -1,10 +1,12 @@
 module github.com/pluja/anysub
 
-go 1.22.3
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
 	entgo.io/ent v0.13.1
-	github.com/a-h/templ v0.2.707
+	github.com/a-h/templ v0.3.857
 	github.com/duke-git/lancet/v2 v2.3.1
 	github.com/dustin/go-humanize v1.0.1
 	github.com/go-resty/resty/v2 v2.13.1
@@ -13,9 +15,10 @@ require (
 	github.com/hibiken/asynq v0.24.1
 	github.com/joho/godotenv v1.5.1
 	github.com/kataras/iris/v12 v12.2.11
+	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.33.0
-	golang.org/x/crypto v0.24.0
+	golang.org/x/crypto v0.36.0
 )
 
 require (
@@ -70,13 +73,13 @@ require (
 	github.com/yosssi/ace v0.0.5 // indirect
 	github.com/zclconf/go-cty v1.14.4 // indirect
 	golang.org/x/exp v0.0.0-20240529005216-23cca8864a10 // indirect
-	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/tools v0.24.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
